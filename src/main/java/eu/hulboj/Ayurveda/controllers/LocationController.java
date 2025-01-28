f1b4a78106780f6b7417b5ece73e602c5e7c7b48
Radoslaw Hulboj <radekhulboj@gmail.com>
2025-01-28 17:44:36 +0100

    kubernets is working

--- src/main/java/eu/hulboj/Ayurveda/controllers/LocationController.java
+++ src/main/java/eu/hulboj/Ayurveda/controllers/LocationController.java
@@ -2,6 +2,7 @@ package eu.hulboj.Ayurveda.controllers;
 
 import eu.hulboj.Ayurveda.entities.Location;
 import eu.hulboj.Ayurveda.services.LocationService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +11,10 @@ import java.util.List;
 @RequestMapping("/api/locations")
 @CrossOrigin(origins = "${ayurveda.allowed.cross.origin}")
 public class LocationController {
+
+    @Value("${ayurveda.allowed.cross.origin}")
+    private String allowedOrigin;
+
     private final LocationService locationService;
 
     public LocationController(LocationService locationService) {
@@ -18,11 +23,13 @@ public class LocationController {
 
     @GetMapping
     public List<Location> getAllLocations() {
+        System.out.println("CORS allowed origin: " + allowedOrigin);
         return locationService.getAllLocations();
     }
 
     @GetMapping("/{id}")
     public Location getLocationById(@PathVariable Long id) {
+        System.out.println("CORS allowed origin: " + allowedOrigin);
         return locationService.getAllLocationById(id);
     }
 }
